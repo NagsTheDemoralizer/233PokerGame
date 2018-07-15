ccda9c81d4245d38dca764f6503e252536ab2a89
NagsTheDemoralizer <34460480+NagsTheDemoralizer@users.noreply.github.com>
2018-07-15 15:48:47 -0600

    Add files via upload

---
+++ PokerServer/Game.java
@@ -0,0 +1,21 @@
+public class Game {
+
+    public String ActivePlayers;
+
+
+    // start betPhase
+    private void startBetPhase() {
+
+        String playersCopy = activePlayers;
+        int[] numCalls = new int[playersCopy.length()];
+
+        /* */
+
+
+
+
+    }
+
+
+
+}
